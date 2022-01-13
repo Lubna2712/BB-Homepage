0fd9e6fa32d3d0a84830117de683caf5e014953c
Lubna Khan <klubna17@gmail.com>
2022-01-13 23:29:35 +0530

    Hero Section in Desktop updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import HeroSection from './components/HeroSection'
 
 function App() {
   return (
     <div className="App">
-      <Header>
-
-      </Header>
+      <Header />
+      <HeroSection />
     </div>
   );
 }
